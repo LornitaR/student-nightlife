d7bb457f7d65e746046f49ddb575d2e21178727d
Lorna Ryan <lornaniriain@gmail.com>
2019-01-10 19:47:06 -0500

    set up initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BarsComponent } from './bars/bars.component';
+import { DrinksComponent } from './drinks/drinks.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarsComponent,
+    DrinksComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
